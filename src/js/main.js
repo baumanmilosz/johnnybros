1dc4cc3e8b54ca069f2171462253e1a70b5536a3
Milosz Bauman <bauman.milosz@gmail.com>
2019-06-19 14:58:34 +0200

    Finished mobile version

---
+++ src/js/main.js
@@ -0,0 +1,6 @@
+const hamburgerBtn = document.querySelector('.hamburger');
+const navBar = document.querySelector('.nav-bar');
+
+hamburgerBtn.addEventListener('click', () => {
+  navBar.classList.toggle('opened');
+})
